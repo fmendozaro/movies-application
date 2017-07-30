17bad1c2493c28c1e7f6b8fbb5a85e449f07645c
Fernando <fernandoza@gmail.com>
2017-07-29 20:24:35 -0500

    Added the html elements to display the movies in the page.

--- src/index.js
+++ src/index.js
@@ -10,11 +10,21 @@ sayHello('World');
 const getMovies = require('./getMovies.js');
 
 getMovies().then((movies) => {
+
   console.log('Here are all the movies:');
+  let moviesDiv = document.getElementById("movies");
+  let moviesHMTL = "";
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
+    moviesHMTL += `<div>
+                    <span class="movie-title">Title: ${title}</span>
+                    <span class="rating">Rating: ${rating}</span>
+                   </div>`;
   });
+
+  moviesDiv.innerHTML = moviesHMTL ;
+
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
+    let errorDiv = document.getElementById("error");
+    errorDiv.innerText = 'Oh no! Something went wrong.\nCheck the console for details.';
 });
