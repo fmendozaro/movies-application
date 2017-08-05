ab09f9926ab64f82d2fd2a1a623089942ea932ad
Fernando <fernandoza@gmail.com>
2017-08-05 18:24:55 -0500

    Create Overlay for loading content.
    - CSS for the overlay.
    - Implement in the js.

--- src/index.js
+++ src/index.js
@@ -9,20 +9,24 @@ sayHello('World');
  */
 const getMovies = require('./getMovies.js');
 
+let overlay = document.getElementById("overlay")
+overlay.style.setProperty("display", "block");
+
 getMovies().then((movies) => {
 
-  console.log('Here are all the movies:');
-  let moviesDiv = document.getElementById("movies");
-  let moviesHMTL = "";
-  movies.forEach(({title, rating, id}) => {
+    console.log('Here are all the movies:');
+    let moviesDiv = document.getElementById("movies");
+    let moviesHMTL = "";
+    movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
     moviesHMTL += `<div>
                     <span class="movie-title">Title: ${title}</span>
                     <span class="rating">Rating: ${rating}</span>
                    </div>`;
-  });
+    });
 
-  moviesDiv.innerHTML = moviesHMTL ;
+    moviesDiv.innerHTML = moviesHMTL ;
+    overlay.style.setProperty("display", "none");
 
 }).catch((error) => {
     let errorDiv = document.getElementById("error");
