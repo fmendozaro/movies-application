cd6dab9dc84feeb03db99d6a5363fba1a8ccda77
Fernando <fernandoza@gmail.com>
2017-09-14 10:07:18 -0500

    Changes the look and feel of the page.

--- src/index.js
+++ src/index.js
@@ -8,42 +8,47 @@ sayHello('World');
  * require style imports
  */
 const getMovies = require('./getMovies.js');
+const movieActions = require("./movieActions");
 
-let overlay = document.getElementById("overlay");
-let showBtn = document.getElementById("show-add-form");
-let addForm = document.getElementById("add-movie-form");
-let addMovie = document.getElementById("add-movie");
+const overlay = document.getElementById("overlay");
+const addMovie = document.getElementById("add-movie-btn");
+const moviesBody = document.getElementById("moviesList");
+const errorDiv = document.getElementById("error");
+
 overlay.style.setProperty("display", "block");
 
-getMovies().then((movies) => {
+function buildTable(){
 
-    let moviesDiv = document.getElementById("moviesList");
-    let moviesHMTL = "";
-    movies.forEach(({title, rating, id}) => {
-    moviesHMTL += `<tr>
-                        <td>Title: ${title}</span>
+    getMovies().then((movies) => {
+
+        overlay.style.setProperty("display", "block");
+
+        let moviesHMTL = "";
+        movies.forEach(({title, rating, id}) => {
+            moviesHMTL += `
+                    <tr>
+                        <td>${title}</td>
                         <td class="rating">${rating}</td>
-                   </tr>`;
+                    </tr>`;
+        });
+
+        moviesBody.innerHTML = moviesHMTL ;
+        overlay.style.setProperty("display", "none");
+
+    }).catch((error) => {
+        errorDiv.innerText = 'Oh no! Something went wrong.\nCheck the console for details.';
+        overlay.style.setProperty("display", "none");
     });
+}
 
-    moviesDiv.innerHTML = moviesHMTL ;
-    overlay.style.setProperty("display", "none");
-
-}).catch((error) => {
-    let errorDiv = document.getElementById("error");
-    errorDiv.innerText = 'Oh no! Something went wrong.\nCheck the console for details.';
-    overlay.style.setProperty("display", "none");
-});
-
-showBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log("click");
-    addForm.classList.remove("hidden");
-});
+buildTable();
 
 addMovie.addEventListener("click", (e) => {
     e.preventDefault();
-    let title = document.getElementById("title");
-    let rating = document.getElementsByName("rating");
-    
+    let title = document.getElementById("title").value;
+    let ratings = document.getElementsByName("rating");
+    let rating = Array.from(ratings).filter( (radio) => radio.checked === true );
+    movieActions.addMovie(title, rating[0].value, moviesBody);
+    buildTable();
+    $('#form-modal').modal('hide');
 });
